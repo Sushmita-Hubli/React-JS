e70b08ced74afadc9fa2f79c1062749e028ca7f9
Hubli-Sushmita <125744066+Sushmita-Hubli@users.noreply.github.com>
2025-01-07 11:28:48 -0500

    event and event handler in React

--- fifteen-app/src/ClickFunction.js
+++ fifteen-app/src/ClickFunction.js
@@ -12,7 +12,7 @@ function ClickFunction() {
 }
   return (
     <div className='App'>
-      <button onClick={HelloFunc}>Click</button> {/*we dont apply parenthesis in front of function name*/}
+      <button onClick={()=>HelloFunc()}>Click</button> {/*we dont apply parenthesis in front of function name or we can pass an arrow function inside which can point to a function which has paranthesis. note that we donot write this keyword because our component is a fiunction component*/}
     </div>
   )
 }
@@ -32,7 +32,7 @@ export class ClickFunction1 extends React.Component {
    {
     return (
       <div className='App'>
-        <button onClick={this.HeluFunc}>Click1</button>
+        <button onClick={this.HeluFunc}>Click1</button> {/*here we can write either onClick={this.HeluFunc} or onClick={()=>this.HeluFunc()} . these both things signify the same thing. also if the function HeluFunc is made inside the render function, then we need not have to write this keyword in front of class name . but in this case , we have made the function outside the render function so we will have to use this keyword.*/}
       </div>
     )
   }
