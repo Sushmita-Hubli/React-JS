57765237778604801e68ce9ab3b11254bbbcfe15
Hubli-Sushmita <125744066+Sushmita-Hubli@users.noreply.github.com>
2025-01-08 14:28:03 -0500

    Passing Methods as props

--- twentythree-app/src/ChildComponent.js
+++ twentythree-app/src/ChildComponent.js
@@ -7,7 +7,8 @@ export default class ChildComponent extends Component {
       <h1>This is child class</h1>
       <button onClick={this.props.callMethod}>Click</button>{/*here we have passed function without parameter*/}
       <button onClick={()=>this.props.callMethod1("Sushmita")}>Click</button>{/*here we have passed function with parameter*/}
-      
+      <h3>For functions without parameters, you can directly pass the function reference.
+      For functions with parameters, you need to wrap the call in an arrow function to prevent it from executing immediately during render.</h3>
         
       </div>
     )
