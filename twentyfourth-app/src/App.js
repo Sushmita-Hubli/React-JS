630ee804b2e07d204f1ad52bf947654d596f4f07
Hubli-Sushmita <125744066+Sushmita-Hubli@users.noreply.github.com>
2025-01-08 15:48:39 -0500

    conditional rendering in React

---
+++ twentyfourth-app/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import Component1 from './Component1';
+import Component2 from './Component2';
+
+function App() {
+  let name="Sushmita";
+  let data;
+  if(name=="Sushmita")
+  {
+    data= <Component1/>
+
+  }
+  else
+  {
+    data=<Component2/>
+  }
+  const age=22;
+
+  return (
+    <>
+    {data}
+    {name=="Sushmita" && <h1>Hello Sushmita</h1>}  {/*its like if name= sushmita then h1 will be printed . if name!= sushmita then nothing will be printed or rendered in the DOM*/}
+    {age>18?<h1>can vote</h1>:<h1>cannot vote</h1>}
+
+    </>
+  );
+}
+
+export default App;
