5fe8fa402827ee37a0efed9e17344ec35ec3a0de
Hubli-Sushmita <125744066+Sushmita-Hubli@users.noreply.github.com>
2025-01-08 17:34:21 -0500

    conditional rendering

--- twentyfourth-app/src/App.js
+++ twentyfourth-app/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Component1 from './Component1';
 import Component2 from './Component2';
+import NewComponent from './NewComponent';
 
 function App() {
   let name="Sushmita";
@@ -22,6 +23,7 @@ function App() {
     {data}
     {name=="Sushmita" && <h1>Hello Sushmita</h1>}  {/*its like if name= sushmita then h1 will be printed . if name!= sushmita then nothing will be printed or rendered in the DOM*/}
     {age>18?<h1>can vote</h1>:<h1>cannot vote</h1>}
+    <NewComponent/>
 
     </>
   );
