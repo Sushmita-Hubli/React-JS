a3f7da35bbb78fa7bb1efe6476cf49f30b2c25a3
Sushmita <hublisushmita7@gmail.com>
2025-08-14 00:27:27 -0400

    Navigating programatically
    Navigation on Button Click
    
    Navigation Inside Event Handler
    
    Navigating to multiple Pages with single event handler
    
    Condition based Navigation
    
    Go Back Button

---
+++ fiftyfive-app/src/App.js
@@ -0,0 +1,52 @@
+import logo from './logo.svg';
+import './App.css';
+import {Routes,Route,useNavigate} from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';
+
+function App() {
+  const navigate=useNavigate();
+
+  const NavigateToAbout =()=>{
+    navigate('/about');
+  }
+
+  const NavigateTo=(url)=>{
+    navigate(url);
+  }
+
+  const NavigateToWhere=()=>{
+    let name='sushmita';
+    if(name=='sush')
+    {
+      navigate('/about');
+    }
+    else 
+    {
+      navigate('/contact');
+    }
+  }
+  return (
+    <>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+    </Routes>
+    {/* <button onClick={()=>navigate('/about')}>About</button> */}
+<button onClick={()=>navigate('/contact')}>Contact</button>
+<button onClick={()=>NavigateToAbout()}>About</button>
+<button onClick={()=>NavigateTo('/about')}>About with NavigateTo</button>
+<button onClick={()=>NavigateToWhere()}>Click Me</button>
+<br></br>
+<button onClick={()=>navigate(-1)}>Go Back</button>
+   
+    </>
+    
+  );
+}
+
+export default App;
