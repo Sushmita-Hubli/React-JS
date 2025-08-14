4eebe4d31fb0c7a5cf24710c68781f4b65cc5432
Sushmita <hublisushmita7@gmail.com>
2025-08-13 21:59:58 -0400

    NavLink

---
+++ fiftyfour-app/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      Link → Navigates to a different route in your React app (basic navigation).
+<br></br>
+NavLink → Works like Link but adds styling (like an active class) automatically when the link matches the current URL — useful for highlighting the active page in a navbar.
+    </div>
+  )
+}
+
+export default Home
