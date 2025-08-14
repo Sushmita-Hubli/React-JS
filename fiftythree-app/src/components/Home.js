b7eba49c4a2486cb58d94279a3e82b9e8f67b23a
Sushmita <hublisushmita7@gmail.com>
2025-08-13 21:10:47 -0400

    Navbar and Link

---
+++ fiftythree-app/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Here now we have created a seperate component named Navbar and there we have used 'Link' tag with prop 'to'</p>
+    </div>
+  )
+}
+
+export default Home
